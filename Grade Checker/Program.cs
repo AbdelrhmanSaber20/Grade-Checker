482e7b6d229407dda7398298772091608cf0b27a
dell <dell@DESKTOP-HP2VAJ1>
2025-08-02 21:54:01 +0300

    Add project files.

---
+++ Grade Checker/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Grade_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.Write("Enter Your Grade : ");
+         int Grade = Convert.ToInt32(Console.ReadLine());
+         if (Grade < 60) { Console.WriteLine("Your Grade Is = F"); }
+         else if (Grade >= 60 && Grade <= 69) { Console.WriteLine("Your Grade Is = D");}
+         else if (Grade >= 70 && Grade <= 79) { Console.WriteLine("Your Grade Is = C");}
+         else if (Grade >= 80 && Grade <= 89) { Console.WriteLine("Your Grade Is = B");}
+         else if (Grade >= 90 && Grade <= 100) { Console.WriteLine("Your Grade Is = A");}
+        }
+    }
+}
