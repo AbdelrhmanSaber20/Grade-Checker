a5cc194e644a7339e4999d2e974fefdc62a65fb8
AbdelrhmanSaber20 <abdelrhmansabereldesoki@gmail.com>
2025-08-02 22:26:25 +0300

    Add files via upload

---
+++ Num X Num/Num X Num/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Num_X_Num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your Number = ");
+            Double Num =Convert.ToDouble(Console.ReadLine());
+            int i =0;
+            while (i < Num)
+            {
+                Console.WriteLine("1 * " + Num + " = " + 1 * Num);
+                i = i + 1;
+                Console.WriteLine("2 * " + Num + " = " + 2 * Num);
+                i = i + 1;
+                Console.WriteLine("3 * " + Num + " = " + 3 * Num);
+                i = i + 1;
+                Console.WriteLine("4 * " + Num + " = " + 4 * Num);
+                i = i + 1;
+                Console.WriteLine("5 * " + Num + " = " + 5 * Num);
+                i = i + 1;
+                Console.WriteLine("6 * " + Num + " = " + 6 * Num);
+                i = i + 1;
+                Console.WriteLine("7 * " + Num + " = " + 7 * Num);
+                i = i + 1;
+                Console.WriteLine("8 * " + Num + " = " + 8 * Num);
+                i = i + 1;
+                Console.WriteLine("9 * " + Num + " = " + 9 * Num);
+                i = i + 1;
+                Console.WriteLine("10 * " + Num + " = " + 10 * Num);
+                i = i + 1;
+                Console.WriteLine("11 * " + Num + " = " + 11 * Num);
+                i = i + 1;
+                Console.WriteLine("12 * " + Num + " = " + 12 * Num);
+                i = i + 1;
+
+
+            }
+
+
+                    
+
+
+
+
+
+        }
+    }
+}
